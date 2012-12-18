e66dafcbca78229b6bcbc14af46df6e6f5df530f
ppibburr <ppibburr@laptop.(none)>
2012-12-18 13:12:58 -0500

    ...

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,10 @@
 require 'rubygems'
 require 'minitest/spec'
 require 'minitest/autorun'
-
+require 'simplecov'
+SimpleCov.start do
+  add_filter "/test/"
+end
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 require 'webkit'
